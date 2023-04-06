5775657bfc86b03a173b876ec10fba82c70998d8
Salomon786 <128729041+Salomon786@users.noreply.github.com>
2023-04-06 16:37:10 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,36 @@
-Rover.Move(130)
-basic.pause(2000)
-if (Rover.LineTracking() == 0) {
-    Rover.Move(70)
-} else {
-    if (Rover.LineTracking() == 1) {
-        Rover.MotorRunDual(70, 40)
-        basic.pause(200)
+basic.forever(function () {
+    Rover.Move(130)
+    basic.pause(2000)
+    if (Rover.LineTracking() == 0) {
+        Rover.Move(70)
     } else {
-        if (Rover.LineTracking() == 3) {
+        if (Rover.LineTracking() == 1) {
             Rover.MotorRunDual(70, 40)
             basic.pause(200)
         } else {
-            if (Rover.LineTracking() == 4) {
-                Rover.MotorRunDual(40, 80)
+            if (Rover.LineTracking() == 3) {
+                Rover.MotorRunDual(70, 40)
                 basic.pause(200)
             } else {
-                if (Rover.LineTracking() == 5) {
-                    Rover.MotorRunDual(70, 70)
+                if (Rover.LineTracking() == 4) {
+                    Rover.MotorRunDual(40, 80)
+                    basic.pause(200)
                 } else {
-                    if (Rover.LineTracking() == 6) {
-                        Rover.MotorRunDual(80, 40)
-                        basic.pause(200)
+                    if (Rover.LineTracking() == 5) {
+                        Rover.MotorRunDual(70, 70)
                     } else {
-                        Rover.MotorRunDual(30, 30)
+                        if (Rover.LineTracking() == 6) {
+                            Rover.MotorRunDual(80, 40)
+                            basic.pause(200)
+                        } else {
+                            Rover.MotorRunDual(30, 30)
+                        }
                     }
                 }
             }
         }
     }
-}
+})
 basic.forever(function () {
     for (let index = 0; index < 69; index++) {
         Rover.setRGBLED(Rover.ledIndex(LEDIndex.LED_All), Rover.colors(RoverColors.Red))

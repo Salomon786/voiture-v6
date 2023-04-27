801a33fed62c410c9a55f8f510fa7448f99c7330
Salomon786 <128729041+Salomon786@users.noreply.github.com>
2023-04-27 13:56:30 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,37 +1,4 @@
 basic.forever(function () {
-    Rover.Move(130)
-    basic.pause(2000)
-    if (Rover.LineTracking() == 0) {
-        Rover.Move(70)
-    } else {
-        if (Rover.LineTracking() == 1) {
-            Rover.MotorRunDual(70, 40)
-            basic.pause(200)
-        } else {
-            if (Rover.LineTracking() == 3) {
-                Rover.MotorRunDual(70, 40)
-                basic.pause(200)
-            } else {
-                if (Rover.LineTracking() == 4) {
-                    Rover.MotorRunDual(40, 80)
-                    basic.pause(200)
-                } else {
-                    if (Rover.LineTracking() == 5) {
-                        Rover.MotorRunDual(70, 70)
-                    } else {
-                        if (Rover.LineTracking() == 6) {
-                            Rover.MotorRunDual(80, 40)
-                            basic.pause(200)
-                        } else {
-                            Rover.MotorRunDual(30, 30)
-                        }
-                    }
-                }
-            }
-        }
-    }
-})
-basic.forever(function () {
     for (let index = 0; index < 69; index++) {
         Rover.setRGBLED(Rover.ledIndex(LEDIndex.LED_All), Rover.colors(RoverColors.Red))
         basic.pause(1000)
@@ -40,20 +7,56 @@ basic.forever(function () {
     }
     for (let index = 0; index < 69; index++) {
         basic.showLeds(`
+            . # # # .
             . # . # .
-            # # # # #
-            . # . # .
-            # # # # #
-            . # . # .
+            . # # # .
+            . . . # .
+            . # # # .
             `)
         basic.pause(1000)
         basic.showLeds(`
-            . # # . .
-            . . # . .
-            . . # . .
-            . . # . .
+            . # # # .
+            . # . . .
+            . # # # .
+            . . . # .
             . # # # .
             `)
     }
     music.playMelody("E E - F F - A B ", 180)
+})
+basic.forever(function () {
+    if (Rover.LineTracking() == 0) {
+        Rover.Move(70)
+    } else {
+        if (Rover.LineTracking() == 1) {
+            Rover.MotorRunDual(55, 15)
+            basic.pause(200)
+        } else {
+            if (Rover.LineTracking() == 2) {
+                Rover.MotorRunDual(80, 80)
+                basic.pause(200)
+            } else {
+                if (Rover.LineTracking() == 3) {
+                    Rover.MotorRunDual(55, 15)
+                    basic.pause(200)
+                } else {
+                    if (Rover.LineTracking() == 4) {
+                        Rover.MotorRunDual(15, 55)
+                        basic.pause(200)
+                    } else {
+                        if (Rover.LineTracking() == 5) {
+                            Rover.MotorRunDual(70, 70)
+                        } else {
+                            if (Rover.LineTracking() == 6) {
+                                Rover.MotorRunDual(55, 15)
+                                basic.pause(200)
+                            } else {
+                                Rover.MotorRunDual(40, 40)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 })
